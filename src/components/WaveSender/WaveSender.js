48c6798e1a7a111dfd9a4ac4860d62e2e0fd4558
Victor Forbes <vhforbes@gmail.com>
2022-01-19 20:25:54 -0300

    message functionality

--- src/components/WaveSender/WaveSender.js
+++ src/components/WaveSender/WaveSender.js
@@ -2,73 +2,105 @@ import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import './WaveSender.css';
 import abi from "../../utils/WavePortal.json";
-import { useSelector } from 'react-redux';
 
 const WaveSender = () => {
   const [totalWaves, setTotalWaves] = useState(0)
-
-  const auth = useSelector(state => state.auth)
-
-  const contractAddress = "0xa4eeca7e0aa2a0d5a916843c033a5d25400c4d5b"
+  const [allWaves, setAllWaves] =  useState([])
+  const [message, setMessage] = useState('')
+  const [emptyMessage, setEmptyMessage] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [transactionHash, setTransactionHash] = useState('')
+  const contractAddress = "0xf3DA07C98Fa86A99a242DF3be9bFB6efe69152E2"
   const contractABI = abi.abi
-
+  const { ethereum } = window
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
 
   const getWaves = async () => {
     try {
-      const { ethereum } = window
+      let count = await wavePortalContract.getTotalWaves()
+      setTotalWaves(count.toNumber())
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
-
-        let count = await wavePortalContract.getTotalWaves()
-        setTotalWaves(count.toNumber())
-
-      } else {
-        console.log("eth object doesn't exist: getting waves")
-      }
+  const getAllWaves = async () => {
+    try {
+      let waves = await wavePortalContract.getAllWaves()
+      setAllWaves(waves)
     } catch (err) {
       console.log(err)
     }
   }
 
   const sendWave = async () => {
-    try {
-      const { ethereum } = window
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
-
-        const wave = await wavePortalContract.wave();
-        console.log("Mining...", wave.hash)
+    if (message.length > 0) {
+      setEmptyMessage(false)
+      try {
+        const wave = await wavePortalContract.wave(message);
+        setTransactionHash(wave.hash)
+        setLoading(true)
 
         await wave.wait()
-        console.log("Mined --", wave.hash)
+        setLoading(false)
 
+        getAllWaves()
         getWaves()
-
-      } else {
-        console.log("eth object doesn't exist: getting waves")
+      } catch (err) {
+        console.log(err)
       }
-    } catch (err) {
-      console.log(err)
+    } else {
+      setEmptyMessage(true)
     }
+  }
+
+  const updateMessage = (message) => {
+    setMessage(message)
   }
 
   useEffect(() => {
     getWaves()
+    getAllWaves()
   }, [])
 
   return (
-    <div className="wave-sender">
-      <p className='greeting-text'>Hey! Glad you dropped by! Here you can send me a wave trough the Rinkeby Test Network.</p>
-      <button className='send-wave-btn' onClick={sendWave}>👋</button>
+    <>
+    {loading ? (
+      <div className='loading'>
+        <p>Loading transaction...</p>
+        <p>{transactionHash}</p>
+      </div>
+    )
+    : 
+    (<div className="wave-sender">
+      <p className='greeting-text'>Hey! Glad you dropped by! Here you can send me a wave and a message trough the Rinkeby Test Network.</p>
+      <div className='input-container'>
+        <span className='label'>Leave a message!</span>
+        <input onChange={(e) => updateMessage(e.target.value)} type="text" value={message} className='message-input' />
+        {emptyMessage ? <p className='message-warning'>Please leave a message!</p> : null}
+      </div>
+      <button className='send-wave-btn' onClick={() => sendWave(message)}>👋</button>
       {totalWaves === 0 ? <p>Forbes have no waves</p> : <p>Forbes has {totalWaves} total waves!</p>}
+      <div className='waves-list'>{
+        allWaves.map(wave => (
+          <div className='message-board'>
+            <div className='sender-wallet'>
+              <span>Sent by: </span>
+              <span>{wave[0]}</span>
+            </div>
+            <br/>
+            <span>{wave[1]}</span>
+          </div>
+        ))
+        }
+      </div>
+    </div>)
+    }
+    </>
 
-    </div>
+    
   );
 }
 
